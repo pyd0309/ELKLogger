48d1569f83727f3088ef50256e19b337b856ae4b
pyd0309 <111866158+pyd0309@users.noreply.github.com>
2022-08-27 17:09:31 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="ELKLogging",
-    version="0.0.9",
+    version="0.1.1",
     author="YoungDo Park",
     author_email="pyd0309@gmail.com",
     description="Logging to Logstash/File/Stream",
